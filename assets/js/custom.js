736e545d695ea6ed921b86fb26b4c0031abd378f
lorensansol <lorensansol@gmail.com>
2024-01-24 22:59:47 +0100

    Update: 2024-01-24 22:59:47

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,10 @@
 // Google Analytics Events
-
 document.querySelectorAll('.contact__phone, .contact__whatsapp, .callnow > button').forEach(e => {
   e.addEventListener('click', () => {
-    gtag('event', 'clic', {'event_category': 'llamar', 'event_label': 'podologia'})
+    // eslint-disable-next-line
+    gtag('event', 'clic', {
+      event_category: 'llamar',
+      event_label: 'podologia'
+    })
   })
 })
